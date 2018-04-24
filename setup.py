ca5143a91ca50d23d29ea7e996d1968a97e0ca5a
christopher <cjwright4242@gmail.com>
2018-04-24 13:08:39 -0400

    rename rever things

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def main():
         sys.exit('xonsh currently requires Python 3.4+')
     with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
         readme = f.read()
-    scripts = ['scripts/rever']
+    scripts = ['scripts/orch']
     skw = dict(
         name='orch',
         description='Release Versions of Software',
@@ -29,7 +29,7 @@ def main():
         url='https://github.com/xpdAcq/CI-orchestrator',
         platforms='Cross Platform',
         classifiers=['Programming Language :: Python :: 3'],
-        packages=['rever', 'rever.activities'],
+        packages=['orch'],
         package_dir={'orch': 'orch'},
         package_data={'orch': ['*.xsh']},
         scripts=scripts,
@@ -37,12 +37,12 @@ def main():
         )
     # WARNING!!! Do not use setuptools 'console_scripts'
     # It validates the depenendcies (of which we have none) everytime the
-    # 'rever' command is run. This validation adds ~0.2 sec. to the startup
+    # 'orch' command is run. This validation adds ~0.2 sec. to the startup
     # time of xonsh - for every single xonsh run.  This prevents us from
     # reaching the goal of a startup time of < 0.1 sec.  So never ever write
     # the following:
     #
-    #     'console_scripts': ['rever = rever.main:main'],
+    #     'console_scripts': ['orch = orch.main:main'],
     #
     # END WARNING
     setup(**skw)
