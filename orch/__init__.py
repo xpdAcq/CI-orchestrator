f46ff2d1fd11702965a7c94dbfb790c8a222930b
Christopher J. Wright <cjwright4242@gmail.com>
2019-04-17 08:07:37 -0400

    bumped version to 0.4.0

--- orch/__init__.py
+++ orch/__init__.py
@@ -15,6 +15,6 @@ builtins.__xonsh_env__['RAISE_SUBPROC_ERROR'] = True
 import xonsh.imphooks
 xonsh.imphooks.install_import_hooks()
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 del xonsh, builtins, Execer, Shell
